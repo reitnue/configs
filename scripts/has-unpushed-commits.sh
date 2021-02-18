02b7ada707fd318617fc2fe4545f7477cc43111d
Alex Wang <reitnue@users.noreply.github.com>
2021-02-18 18:42:12 -0500

    Update unpushed commits script

--- scripts/has-unpushed-commits.sh
+++ scripts/has-unpushed-commits.sh
@@ -2,19 +2,16 @@
 # $1 is output when current branch changes are all pushed
 # $2 is output when current branch has unpushed changes
 # $3 is output when current branch has no remote
-git cherry &> /dev/null;
+git cherry > /dev/null 2>&1;
 if [[ $? -ne 0 ]];
 then
     echo $3;
-    return 1;
-fi;
-
-if [[ `git cherry` ]];
-then
-    # has unpushed changes
-    echo $2;
-    return 1;
 else
-    echo $1;
-    return 0;
+    if [[ `git cherry` ]];
+    then
+        # has unpushed changes
+        echo $2;
+    else
+        echo $1;
+    fi;
 fi;
